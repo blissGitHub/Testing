8475bbe377fd6d98e06b1fe9922a963508b3b51c
John Preston <cittamani@gmail.com>
2013-07-13 12:15:16 +0800

    first commit

---
+++ NoteCollector/DetailViewController.h
@@ -0,0 +1,17 @@
+//
+//  DetailViewController.h
+//  NoteCollector
+//
+//  Created by John Preston Cua on 7/12/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DetailViewController : UIViewController <UISplitViewControllerDelegate>
+
+@property (strong, nonatomic) id detailItem;
+
+@property (strong, nonatomic) IBOutlet UILabel *detailDescriptionLabel;
+
+@end
---
+++ NoteCollector/MasterViewController.h
@@ -0,0 +1,22 @@
+//
+//  MasterViewController.h
+//  NoteCollector
+//
+//  Created by John Preston Cua on 7/12/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class DetailViewController;
+
+#import <CoreData/CoreData.h>
+
+@interface MasterViewController : UITableViewController <NSFetchedResultsControllerDelegate>
+
+@property (strong, nonatomic) DetailViewController *detailViewController;
+
+@property (strong, nonatomic) NSFetchedResultsController *fetchedResultsController;
+@property (strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+
+@end
